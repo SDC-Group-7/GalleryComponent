abde495e977973e863fa25f195fde09db8e559cb
Steve Marquez <steveantonio00@gmail.com>
2020-05-02 17:40:06 -0700

    Add async and await to model function makion db.query

--- db/model.js
+++ db/model.js
@@ -1,10 +1,10 @@
 const db = require('./index.js');
 
 module.exports = {
-  gettingProduct: (res) => {
-    db.query('select image_url, product_id, image_id from images inner join reference on image_id = reference.img_id inner join products on product_id = reference.prod_id where prod_id = 66;', (error, data) => {
+  gettingProduct: async (res) => {
+    await db.query('select image_url, product_id, image_id from images inner join reference on image_id = reference.img_id inner join products on product_id = reference.prod_id where prod_id = 66;', (error, data) => {
       if (error) {
-        res.status(400).send(error);
+        res.status(400).end();
       } else {
         res.status(200).json(data);
       }
