d960e9057d98caef4d78adfb03467482d835333b
Steve Marquez <steveantonio00@gmail.com>
2020-05-03 23:10:17 -0700

    Implement callback pattern

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,15 @@
-const model = require('../db/model.js');
+const { getProduct } = require('./model.js');
+
+const getProductImages = (id, callback) => {
+  getProduct(id, (error, results) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, results);
+    }
+  });
+};
 
 module.exports = {
-  getProductImages: async (req, res) => {
-    await model.getProduct(req.params.id, res);
-  },
+  getProductImages,
 };
