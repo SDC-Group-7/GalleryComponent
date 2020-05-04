8fdffd4f166015ed229316cd3b548c0b8607171d
Steve Marquez <steveantonio00@gmail.com>
2020-05-03 23:16:05 -0700

    Fix syntax error and spacing

--- server/start.js
+++ server/start.js
@@ -1,5 +1,5 @@
-const app = require('./index.js');
+const app = require('./server.js');
 
 const port = 3000;
 
-app.listen(port, () => console.log(`APP ---> http://localhost:${port}`));
+app.listen(port, () => console.log(`APP listening on http://localhost:${port}`));
