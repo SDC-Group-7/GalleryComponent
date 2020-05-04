1141ad3e8cecb40abd8978f8d5be6a1b494624c2
Steve Marquez <steveantonio00@gmail.com>
2020-05-03 20:16:16 -0700

    Refactor function to make db query using product id

--- db/model.js
+++ db/model.js
@@ -1,13 +1,13 @@
-// const db = require('./index.js');
+const db = require('./index.js');
 
-// module.exports = {
-//   gettingProduct: async (res) => {
-//     await db.query('select image_url, product_id, image_id from images inner join reference on image_id = reference.img_id inner join products on product_id = reference.prod_id where prod_id = 66;', (error, data) => {
-//       if (error) {
-//         res.status(400).end();
-//       } else {
-//         res.status(200).json(data);
-//       }
-//     });
-//   },
-// };
+module.exports = {
+  getProduct: (id, res) => {
+    db.query(`select image_url, product_id, image_id from images inner join reference on image_id = reference.img_id inner join products on product_id = reference.prod_id where prod_id = ${id}`, (error, data) => {
+      if (error) {
+        res.status(400).end();
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  },
+};
