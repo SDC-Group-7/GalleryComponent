b313b97a9ea720d05d76a62c6fec617d34f399c7
Steve Marquez <steveantonio00@gmail.com>
2020-05-04 20:52:04 -0700

    Implement async/await axios GET request

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,50 @@
 import React, {Component} from 'react';
 import Slider from './Slider.jsx';
 import SideImagesMenu from './SideImagesMenu.jsx';
+import axios from 'axios';
 
 export default class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      data: []
+    }
+    this.getProduct = this.getProduct.bind(this);
+
+    this.handleSetter = this.handleSetter.bind(this);
+    this.handleError = this.handleError.bind(this);
   }
+
+  // Setter
+  handleSetter({data}) {
+    this.setState({data})
+  }
+
+  // Error handler
+  handleError(error) {
+    console.log(error);
+  }
+
+  // GET request
+  async getProduct (id) {
+    await axios.get(`/api/images/${id}`)
+      .then(this.handleSetter)
+      .catch(this.handleError)
+  }
+
+
+  componentDidMount() {
+    // Selecting a random product from 1-100;
+    this.getProduct(Math.floor(Math.random() * 100) + 1);
+  }
+
+
   render( ) {
     return (
       <div className="main">
       <div className="box1">
         <div className="inner-box1">
-          <Slider />
+          <Slider data={this.state.data}/>
         </div>
       </div>
       <div className="box2">
