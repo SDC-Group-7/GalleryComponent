c77c828f5b20f86b120f9281c94f958191914079
Steve Marquez <steveantonio00@gmail.com>
2020-05-06 13:57:40 -0700

    Refactor left & right button to use styled components

--- client/src/components/RightArrow.jsx
+++ client/src/components/RightArrow.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronCircleRight} from '@fortawesome/free-solid-svg-icons';
-
+import styled from "styled-components";
 
 const RightArrow = (props) => {
   return (
     <div>
-      <button className="next right" onClick={props.nextImg}>
+      <S.NextBtn onClick={props.nextImg}>
         <FontAwesomeIcon icon={faChevronCircleRight} size="2x"/>
-      </button>
+      </S.NextBtn>
     </div>
   );
 };
@@ -16,3 +16,17 @@ const RightArrow = (props) => {
 export default RightArrow;
 
 
+
+const S = {};
+S.NextBtn = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  font-size: 18px;
+  color: rgb(91, 88, 88);
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  outline: none;
+  box-shadow: none;
+`;
