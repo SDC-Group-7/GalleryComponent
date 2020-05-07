6b151b450eb568004f17976c390cfaa0e8bc89c1
Steve Marquez <steveantonio00@gmail.com>
2020-05-07 11:43:38 -0700

    Add an extra expect method to check for length of response.body

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,27 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true,
-      "node": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
   },
-  "extends": [
-      "plugin:react/recommended",
-      "plugin:jest/recommended",
-      "airbnb"
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
   ],
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "plugins": [
-      "react",
-      "jest"
+  plugins: [
+    'react',
   ],
-  "settings": {
-      "import/resolver": {
-          "babel-module": {
-              "extensions": [".js", ".jsx"]
-          }
-      }
-  }
-  "rules": {
-      "no-console": 0,
-  }
+  rules: {
+  },
 };
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,16 +12,22 @@ describe('Testing images GET Request', () => {
 
   it('Should get images of a product', async (done) => {
     const productImage = [
-      {
-        image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/7.jpg',
-        product_id: 10,
-        image_id: 5,
-      },
+      { image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/10.jpg', product_id: 10, image_id: 7 },
+      { image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/4.jpg', product_id: 10, image_id: 1 },
+      { image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/10.jpg', product_id: 10, image_id: 7 },
+      { image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/10.jpg', product_id: 10, image_id: 2 },
+      { image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/9.jpg', product_id: 10, image_id: 6 },
+      { image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/7.jpg', product_id: 10, image_id: 5 },
     ];
-    const response = await request.get(`/api/images/${productImage[0].product_id}`);
+
+    const response = await request.get(
+      `/api/images/${productImage[0].product_id}`,
+    );
 
     expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual(productImage);
+    expect(response.body.length).toEqual(6);
+
     done();
   });
 
