c4d8d21b2a8c53dc28de3c2af950d5abbb3e8fb1
Steve Marquez <steveantonio00@gmail.com>
2020-05-09 16:56:24 -0700

    Implement up arrow icon for sidemenu

--- client/src/components/side-menu/TopArrowBtn.jsx
+++ client/src/components/side-menu/TopArrowBtn.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const TopArrowBtn = ({ handleClickUp }) => (
-  <button type="button" onClick={handleClickUp}>Top</button>
+  <button type="button" onClick={handleClickUp}>
+    <FontAwesomeIcon icon={faChevronUp} size="2x" />
+  </button>
 );
 
 export default TopArrowBtn;
