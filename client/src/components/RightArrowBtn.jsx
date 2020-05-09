074d29da01bb96407b8f25746d2d5f2f1216f6a1
Steve Marquez <steveantonio00@gmail.com>
2020-05-08 17:38:56 -0700

    Rename component

---
+++ client/src/components/RightArrowBtn.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const RightArrowBtn = ({ nextImg }) => (
+  <S.Button onClick={nextImg}>
+    <FontAwesomeIcon icon={faChevronCircleRight} size="2x" />
+  </S.Button>
+);
+
+export default RightArrowBtn;
+
+
+const S = {};
+S.Button = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  font-size: 18px;
+  color: rgb(91, 88, 88);
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  outline: none;
+  box-shadow: none;
+`;
