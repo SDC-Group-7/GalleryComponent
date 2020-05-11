9555bfde4c77ec4343a3527b6c49e0046620050d
Steve Marquez <steveantonio00@gmail.com>
2020-05-11 14:58:24 -0700

    Implement envrc

--- db/index.js
+++ db/index.js
@@ -1,5 +1,11 @@
 const mysql = require('mysql');
-const mysqlConfig = require('./config.js');
+
+const mysqlConfig = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+};
 
 const connection = mysql.createConnection(mysqlConfig);
 
