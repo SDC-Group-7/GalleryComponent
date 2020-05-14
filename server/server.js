5998c620803d560eb7facf9d75a1d2b67f49f6bf
Steve Marquez <steveantonio00@gmail.com>
2020-05-14 12:35:03 -0700

    Add a Dockerfile and create app container

--- server/server.js
+++ server/server.js
@@ -27,4 +27,95 @@ app.get('/api/images/:id', (req, res) => {
   });
 });
 
+// app.get('/api/images', (req, res) => {
+//   const dummyData = [
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/20.jpg',
+//       product_id: 1,
+//       image_id: 17,
+//       reference_id: 1,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 5,
+//       reference_id: 2,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/16.jpg',
+//       product_id: 1,
+//       image_id: 4,
+//       reference_id: 3,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/20.jpg',
+//       product_id: 1,
+//       image_id: 17,
+//       reference_id: 4,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 6,
+//       reference_id: 5,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 5,
+//       reference_id: 6,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/20.jpg',
+//       product_id: 1,
+//       image_id: 17,
+//       reference_id: 7,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/20.jpg',
+//       product_id: 1,
+//       image_id: 17,
+//       reference_id: 8,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/10.jpg',
+//       product_id: 1,
+//       image_id: 14,
+//       reference_id: 9,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 3,
+//       reference_id: 10,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 3,
+//       reference_id: 11,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 5,
+//       reference_id: 12,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/6.jpg',
+//       product_id: 1,
+//       image_id: 1,
+//       reference_id: 13,
+//     },
+//     {
+//       image_url: 'https://legogallery.s3-us-west-1.amazonaws.com/2.jpg',
+//       product_id: 1,
+//       image_id: 3,
+//       reference_id: 14,
+//     },
+//   ];
+
+//   res.send(dummyData);
+// });
+
 module.exports = app;
